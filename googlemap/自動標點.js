9a8784e1c2f984b692f099b74b8d66b450b38d67
fung0409 <yyee956754@gmail.com>
2024-10-09 16:06:14 +0800

    Update 自動標點.js

--- googlemap/自動標點.js
+++ googlemap/自動標點.js
@@ -1,4 +1,88 @@
 let map;
+
+async function initMap() {
+  let a;
+  const position = { lat: 25.049212, lng: 121.514012 };
+  const position1 = { lat: 25.049111, lng: 121.514333 };//新增標點
+
+
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  map = new Map(document.getElementById("map"), {
+    zoom: 15,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "",
+  });
+
+  //新增標點
+  const marker1 = new AdvancedMarkerElement({
+    map: map,
+    position: position1,
+    title: "taipai",
+  });
+
+  // 設定資訊視窗（InfoWindow）內容
+  const infoWindowContent = `
+            <div>
+                <h3>台北車站</h3>
+                <p>店家資訊：</p>
+                <ul>
+                    <li>無</li>
+                    <li>無</li>
+                    <li>無</li>
+                    <li>無</li>
+                    <li>無</li>
+                    <li>無</li>
+                    <li>無</li>
+                    <li>無</li>
+                </ul>
+                <p>營業時間：</p>
+                <ul>
+                    <li>無</li>
+                </ul>
+                <p>地址：</p>
+            </div>
+        `;
+         // 建立資訊視窗
+        const infoWindow = new google.maps.InfoWindow({
+          content: infoWindowContent,
+      });
+
+  //點擊顯示店家資訊
+  marker.addListener("click", () => {
+    infoWindow.open(map, marker);
+  });
+  marker1.addListener("click", () => {
+
+    
+    // window.open("標點申請單.html");
+
+
+  });
+  
+}
+
+initMap();
+
+
+
+
+
+
+
+
+
+
+
+//arr標點
+/*let map;
 let marker=[];
 
 async function initMap() {
@@ -21,20 +105,17 @@ async function initMap() {
 //   const marker = new AdvancedMarkerElement({
 //     map: map,
 //     position: position,
-//     title: "Uluru",
 //   });
 
 //   //新增標點
 //   const marker1 = new AdvancedMarkerElement({
 //     map: map,
 //     position:position,
-//     title: "taipai",
 //   });
   
 //   const marker2 = new AdvancedMarkerElement({
 //     map: map,
 //     position: position2,
-//     title: "taipai",
 //   });
 
   for (let i=0; i<=5; i++){
@@ -55,3 +136,4 @@ function addmarker(a){
 
 initMap();
 
+*/
